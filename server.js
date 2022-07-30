10f5dc3ec1a8c388c463850c1e2901abb416f7ca
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 03:45:49 -0500

    adding app.use()

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'client')));
