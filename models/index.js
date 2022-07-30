8066cb9caaecf40c374d88444baca927db3d11d8
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 05:15:55 -0500

    setting up models

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Reaction = require('./Reaction');
+
+
+
+
+
+module.exports = { User, Post, Comment, Reaction };
