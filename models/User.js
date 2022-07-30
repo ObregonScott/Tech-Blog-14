5e7460c45f56469e5adfb9a073b29b1bfcbeba18
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 10:41:14 -0500

    redoing User

--- models/User.js
+++ models/User.js
@@ -1,63 +1,90 @@
-const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
-const Reaction = require('./Reaction');
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
-User.hasMany(Post, {
-    foreignKey: 'user_id'
-});
+class User extends Model {
 
-Post.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+}
 
-User.belongsToMany(Post, {
-    through: Reaction,
-    as: 'reacted_posts',
-    foreignKey: 'user_id'
-});
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
 
-Post.belongsToMany(User, {
-    through: Reaction,
-    as: 'reacted_posts',
-    foreignKey: 'post_id'
-})
+        },
+        password: {
 
-Reaction.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+            validate: {
+                len: [4]
+            }
 
-Reaction.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
+        }
+    },
+    {
 
-Reaction.belongsTo(Comment, {
-    foreignKey: 'comment_id'
-})
+    }
+)
 
-Post.hasMany(Reaction, {
-    foreignKey: 'post_id'
-});
+// User.hasMany(Post, {
+//     foreignKey: 'user_id'
+// });
 
-Comment.hasMany(Reaction, {
-    foreignKey: 'comment_id'
-});
+// Post.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+// User.belongsToMany(Post, {
+//     through: Reaction,
+//     as: 'reacted_posts',
+//     foreignKey: 'user_id'
+// });
 
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
+// Post.belongsToMany(User, {
+//     through: Reaction,
+//     as: 'reacted_posts',
+//     foreignKey: 'post_id'
+// })
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id'
-});
+// Reaction.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
 
-Post.hasMany(Comment, {
-    foreignKey: 'post_id'
-})
+// Reaction.belongsTo(Post, {
+//     foreignKey: 'post_id'
+// });
+
+// Reaction.belongsTo(Comment, {
+//     foreignKey: 'comment_id'
+// })
+
+// Post.hasMany(Reaction, {
+//     foreignKey: 'post_id'
+// });
+
+// Comment.hasMany(Reaction, {
+//     foreignKey: 'comment_id'
+// });
+
+// Comment.belongsTo(User, {
+//     foreignKey: 'user_id'
+// });
+
+// Comment.belongsTo(Post, {
+//     foreignKey: 'post_id'
+// });
+
+// User.hasMany(Comment, {
+//     foreignKey: 'user_id'
+// });
+
+// Post.hasMany(Comment, {
+//     foreignKey: 'post_id'
+// })
 
 
-module.exports = { User, Post, Comment, Reaction };
+module.exports = User;
