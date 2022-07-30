8874b5f149e269cb9d3bcb0ccc6698a1722b5d4b
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 03:46:50 -0500

    added handlebars

--- server.js
+++ server.js
@@ -16,3 +16,4 @@ const PORT = process.env.PORT || 3001
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'client')));
+app.engine('handlebars', hbs.engine);
