63df169f78b69a9dd087564bc8f37c1385a13d58
Scott Obregon <obregon.scott@gmail.com>
2022-07-31 16:14:03 -0500

    push for BCS Tutor

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-const userRoutes = require('./user_routes');
-const postRoutes = require('./post_routes');
-const commentRoutes = require('./comment_routes');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
-router.use('/users', userRoutes);
+// router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
